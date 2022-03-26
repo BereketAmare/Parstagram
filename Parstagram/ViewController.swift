9391cd7a1fff61281cdc55e5bd1e7bb0af948843
Bereket Nigussie <48007960+BereketAmare@users.noreply.github.com>
2022-03-26 01:18:55 -0400

    added Parstagram files

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Bereket Nigussie on 3/25/22.
+//
+
+import UIKit
+import ParseSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var p = PFObject(
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
